2e2d5b03b0251748831959e6918382b5b7c72db3
Beads <beads@beadsland.com>
2012-05-12 12:21:53 -0400

    add <br> before ordered and unordered lists
    carriage return before unordered and ordered lists

--- src/edown_xmerl.erl
+++ src/edown_xmerl.erl
@@ -29,8 +29,8 @@
 %% Note: we assume XML data, so all tags are lowercase!
 
 -export(['#root#'/4,
-	 '#element#'/5,
-	 '#text#'/1]).
+  '#element#'/5,
+  '#text#'/1]).
 
 -import(xmerl_lib, [markup/3, find_attribute/2, export_text/1]).
 
@@ -60,10 +60,10 @@ brstrip(Str) -> re:replace(Str, "\\s+\\s\$", "", [global, multiline]).
 
 '#root#'(Data, Attrs, [], _E) ->
     case find_attribute(header, Attrs) of
-	{value, Hdr} ->
-	    [lists:flatten(io_lib:fwrite("HEADER: ~p~n", [Hdr])), Data];
-	false ->
-	    Data
+  {value, Hdr} ->
+      [lists:flatten(io_lib:fwrite("HEADER: ~p~n", [Hdr])), Data];
+  false ->
+      Data
     end.
 
 %% Note that SGML does not have the <Tag/> empty-element form.
@@ -74,17 +74,17 @@ brstrip(Str) -> re:replace(Str, "\\s+\\s\$", "", [global, multiline]).
 
 '#element#'('pre_pre', Data, Attrs, Parents, E) ->
     case re:run(Data, "<a href=", []) of
-	{match, _} ->
-	    '#element#'(pre, Data, Attrs, Parents, E);
-	nomatch ->
-	    '#element#'(pre, escape_pre(Data), Attrs, Parents, E)
+  {match, _} ->
+      '#element#'(pre, Data, Attrs, Parents, E);
+  nomatch ->
+      '#element#'(pre, escape_pre(Data), Attrs, Parents, E)
     end;
 '#element#'('esc_tt', Data, Attrs, Parents, E) ->
     case within_html(Parents) of
-	true ->
-	    '#element#'(tt, escape_pre(Data), Attrs, Parents, E);
-	false ->
-	    '#element#'(tt, Data, Attrs, Parents, E)
+  true ->
+      '#element#'(tt, escape_pre(Data), Attrs, Parents, E);
+  false ->
+      '#element#'(tt, Data, Attrs, Parents, E)
     end;
 '#element#'('pre', Data, Attrs, Parents, E) ->
     xmerl_html:'#element#'('pre', Data, Attrs, Parents, E);
@@ -95,10 +95,10 @@ brstrip(Str) -> re:replace(Str, "\\s+\\s\$", "", [global, multiline]).
     Data;
 '#element#'(a, Data, Attrs, Parents, E) ->
     case edown_lib:redirect_uri(E) of
-	false ->
-	    elem(a, Data, Attrs, Parents, E);
-	#xmlElement{attributes = Attrs1, parents = Parents1} = E1 ->
-	    elem(a, Data, Attrs1, Parents1, E1)
+  false ->
+      elem(a, Data, Attrs, Parents, E);
+  #xmlElement{attributes = Attrs1, parents = Parents1} = E1 ->
+      elem(a, Data, Attrs1, Parents1, E1)
     end;
 '#element#'(Tag, Data, Attrs, Parents, E) ->
     elem(Tag, Data, Attrs, Parents, E).
@@ -111,10 +111,10 @@ elem(localdef, Data, Attrs, Parents, E) ->
     alias_for(li, Data, Attrs, Parents, E);
 elem(Tag, Data, Attrs, Parents, E) ->
     case needs_html(Tag, Attrs) orelse within_html(Parents) of
-	true ->
-	    html_elem(Tag, Data, Attrs, Parents, E);
-	false ->
-	    md_elem(Tag, Data, Attrs, Parents, E)
+  true ->
+      html_elem(Tag, Data, Attrs, Parents, E);
+  false ->
+      md_elem(Tag, Data, Attrs, Parents, E)
     end.
 
 escape_pre(Data) ->
@@ -125,30 +125,30 @@ alias_for(Tag, Data, Attrs, Parents, E) ->
 
 html_elem(Tag, Data, Attrs, Parents, E) ->
     HTML = fun() ->
-		   xmerl_html:'#element#'(Tag, Data, Attrs, Parents, E)
-	   end,
+      xmerl_html:'#element#'(Tag, Data, Attrs, Parents, E)
+    end,
     case within_html(Parents) of
-	true ->
-	    HTML();
-	false ->
-	    ["\n\n", HTML(), "\n\n"]
+  true ->
+      HTML();
+  false ->
+      ["\n\n", HTML(), "\n\n"]
     end.
 
 md_elem(a, Data, Attrs, _Parents, _E) ->
     %% io:fwrite("A TAG = ~p~nPs = ~p~n", [_E, _Parents]),
     case lists:keyfind(href, #xmlAttribute.name, Attrs) of
-	#xmlAttribute{value = HRef}  ->
-	    ["[", Data, "](", HRef, ")"];
-	false ->
-	    case lists:keyfind(name, #xmlAttribute.name, Attrs) of
-		#xmlAttribute{} ->
-		    [ %%"\n",
-		     xmerl_lib:start_tag(a,Attrs),
-		     Data,
-		     xmerl_lib:end_tag(a)
-		      %%"\n"]
-		      ]
-	    end
+  #xmlAttribute{value = HRef}  ->
+      ["[", Data, "](", HRef, ")"];
+  false ->
+      case lists:keyfind(name, #xmlAttribute.name, Attrs) of
+    #xmlAttribute{} ->
+        [ %%"\n",
+        xmerl_lib:start_tag(a,Attrs),
+        Data,
+        xmerl_lib:end_tag(a)
+          %%"\n"]
+          ]
+      end
     end;
 md_elem(img, _Data, Attrs, _Parents, _E) ->
     #xmlAttribute{value = Src} = lists:keyfind(src,#xmlAttribute.name,Attrs),
@@ -160,55 +160,55 @@ md_elem(li, Data, _Attrs, [{ol,_}|_], _E) ->
     ["1. ", strip(Data), "\n"];
 md_elem(Tag, Data, Attrs, Parents, E) ->
     case Tag of
-	title ->
-	    %% io:fwrite("TITLE = |~s|~n", [Data]),
-	    Str = lists:flatten(Data),
-	    [Str, "\n", [$= || _ <- to_string(Str)], "\n"];
-	html  -> Data;
-	body  -> Data;
-	'div' -> Data;
-	ul    -> Data;
-	ol    -> Data;
-	p     -> ["\n\n", Data];
-	b     -> ["__", no_nl(Data), "__"];
-	em    -> ["_", no_nl(Data), "_"];
-	i     -> ["_", no_nl(Data), "_"];
-	tt    -> ["`", no_nl(Data), "`"];
-	code  ->
-	    %% edoc_macros.erl hard-codes expansion of the {@type ...} macro
-	    %% as a HTML href inside <code>...</code>
-	    case re:run(Data, "<a href=", []) of
-		{match,_} ->
-		    %% ["<code>", no_nl(Data), "</code>"];
-		    ["<code>", no_nl(Data), "</code>"];
-		_ ->
-		    %% ["`", no_nl(Data), "`"]
-		    %% Don't strip newlines here, as it messes up the specs
-		    ["`", Data, "`"]
-	    end;
-	dl    -> Data;
-	dt    -> html_elem(dt, Data, Attrs, Parents, E);
-	dd    -> html_elem(dd, Data, Attrs, Parents, E);
-	h1 -> ["\n\n#", no_nl(Data), "#\n"];
-	h2 -> ["\n\n##", no_nl(Data), "##\n"];
-	h3 -> ["\n\n###", no_nl(Data), "##\n"];
-	h4 -> ["\n\n####", no_nl(Data), "##\n"];
-	hr -> "---------\n";
-	head -> [];
-	_ ->
-		    ["\n",
-		     xmerl_lib:start_tag(Tag,Attrs),
-		     Data,
-		     xmerl_lib:end_tag(Tag),
-		    "\n"]
+  title ->
+      %% io:fwrite("TITLE = |~s|~n", [Data]),
+      Str = lists:flatten(Data),
+      [Str, "\n", [$= || _ <- to_string(Str)], "\n"];
+  html  -> Data;
+  body  -> Data;
+  'div' -> Data;
+  ul    -> ["\n", Data];
+  ol    -> ["\n", Data];
+  p     -> ["\n\n", Data];
+  b     -> ["__", no_nl(Data), "__"];
+  em    -> ["_", no_nl(Data), "_"];
+  i     -> ["_", no_nl(Data), "_"];
+  tt    -> ["`", no_nl(Data), "`"];
+  code  ->
+      %% edoc_macros.erl hard-codes expansion of the {@type ...} macro
+      %% as a HTML href inside <code>...</code>
+      case re:run(Data, "<a href=", []) of
+    {match,_} ->
+        %% ["<code>", no_nl(Data), "</code>"];
+        ["<code>", no_nl(Data), "</code>"];
+    _ ->
+        %% ["`", no_nl(Data), "`"]
+        %% Don't strip newlines here, as it messes up the specs
+        ["`", Data, "`"]
+      end;
+  dl    -> Data;
+  dt    -> html_elem(dt, Data, Attrs, Parents, E);
+  dd    -> html_elem(dd, Data, Attrs, Parents, E);
+  h1 -> ["\n\n#", no_nl(Data), "#\n"];
+  h2 -> ["\n\n##", no_nl(Data), "##\n"];
+  h3 -> ["\n\n###", no_nl(Data), "##\n"];
+  h4 -> ["\n\n####", no_nl(Data), "##\n"];
+  hr -> "---------\n";
+  head -> [];
+  _ ->
+        ["\n",
+        xmerl_lib:start_tag(Tag,Attrs),
+        Data,
+        xmerl_lib:end_tag(Tag),
+        "\n"]
     end.
 
 within_html(Tags) ->
     lists:any(fun({pre,_}) -> true;
-		 ({pre_pre,_}) -> true;
-		 ({code,_}) -> true;
-		 ({T,_}) -> needs_html(T)
-	      end, Tags).
+    ({pre_pre,_}) -> true;
+    ({code,_}) -> true;
+    ({T,_}) -> needs_html(T)
+        end, Tags).
 
 needs_html(T) ->
     needs_html(T, []).
@@ -218,7 +218,7 @@ needs_html(T, _Attrs) ->
 
 no_nl(S) ->
     string:strip([C || C <- to_string(S),
-		       C =/= $\n], both).
+          C =/= $\n], both).
 
 %% attr(#xmlAttribute{name = N, value = V}) ->
 %%     "(" ++ atom_to_list(N) ++ "=" ++ [a_val(V)] ++ ")".
